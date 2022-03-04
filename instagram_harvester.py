4d0b5f5156a602d34c44aa84f29b081e4ee0912f
Frederik Gremler <frederik.gremler@gmail.com>
2022-03-04 11:44:43 +0100

    Change all procedure to new package instagram_scraper

--- instagram_harvester.py
+++ instagram_harvester.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from instascrape import Profile, scrape_posts
+import instagram_scraper
 
 import logging
 import re
@@ -14,6 +15,7 @@ from io import BytesIO
 import warcprox
 import random
 import time
+import pickle
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
@@ -49,72 +51,6 @@ class InstagramHarvester(BaseHarvester):
         self.harvest_media_types = { 'photo': True }
         self.local = False # todo
 
-
-    def initiate_selenium_webdriver_local(self):
-        """
-        Instantiates local selenium webdriver without docker.
-        """
-        driver = webdriver.Firefox(executable_path=r'/mnt/c/Users/Frederik Gremler/Documents/EO2/harvester/selenium/geckodriver.exe')
-        return driver
-
-    def initiate_selenium_webdriver(self):
-        """
-        Instantiates selenium webdriver with docker connection for sfm
-        Closing should take place outside of this function! (reused from sfm-facebookharvester)
-        """
-        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('headless')
-        chrome_options.add_argument('start-maximised')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--window-size=1200x800')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument(f"user-agent={user_agent}")
-
-        # this will connect to the selenium container starting scraping
-        # Note: host name of the running container is "selenium"
-        driver = webdriver.Remote("http://selenium:4444/wd/hub", {'browserName': 'chrome'}, options=chrome_options)
-        return driver
-
-    def insta_login(self, driver):
-        """Logs into instagram and returns the respective webdriver session via selenium.
-        Closing has to happen elsewhere!"""
-
-        user_email = self.message['credentials']['user_email_ins']
-        user_password = self.message['credentials']['user_password_ins']
-
-        driver.get(BASE_INSTAGRAM_URL)
-        driver.maximize_window()
-
-        # accept cookies
-        cookies = driver.find_element_by_css_selector("button.aOOlW:nth-child(2)")
-
-        cookies.click()
-
-        time.sleep(random.uniform(3,9))
-
-        username_ins = driver.find_element_by_name('username')
-        password_ins = driver.find_element_by_name('password')
-        submit = driver.find_element_by_css_selector("button[type='submit']")
-
-        time.sleep(random.uniform(3,9))
-
-        # send keys and make sure not prepolutaed
-        # 2fa has to be deactivated
-        username_ins.clear()
-        password_ins.clear()
-        username_ins.send_keys(user_email)
-        password_ins.send_keys(user_password)
-
-        time.sleep(random.uniform(3,9))
-        # Step 4) Click Login
-        submit.click()
-
-        time.sleep(random.uniform(3,9))
-
-        return(driver)
-
     def harvest_seeds(self):
 
         harvest_type = self.message.get("type")
@@ -135,7 +71,7 @@ class InstagramHarvester(BaseHarvester):
 
         for seed in self.message.get("seeds", []):
             print(seed)
-            self.instagram_user_timeline(username = seed.get('token'))
+            self.instagram_user_timeline(username=seed.get('token'))
 
     def instagram_user_timeline(self, username):
         """Scrapes single user's post """
@@ -143,60 +79,11 @@ class InstagramHarvester(BaseHarvester):
         incremental = self.message.get("options", {}).get("incremental", False)
         harvest_media = self.message.get("options", {}).get("harvest_media", False)
 
-        # if no cookies exist, get them
-        if not os.path.exists("/tmp/cookies.json"):
-            driver = self.initiate_selenium_webdriver()
-            self.insta_login(driver = driver)
-
-            cookies = driver.get_cookies()
-
-            with open("/tmp/cookies.json", "w") as f:
-                json.dump(cookies, f)
-
-
-        # prepare selenium session and login
-        if self.local:
-            driver = self.initiate_selenium_webdriver_local()
-        else:
-            driver = self.initiate_selenium_webdriver()
-
-        # after this cookies should be present but check anyways
-        # check whether cookies are present, otherwise try to
-        # log in
-        if os.path.isfile("/tmp/cookies.json"):
-            # first navigate to fb, otherwise
-            # selenium does not accept the cookies
-            # navigate to page
-            driver.get("https://www.instagram.com/")
-
-            #  load cookies
-            with open("/tmp/cookies.json") as f:
-                cookies = json.load(f)
-
-            # add to driver
-            for cookie in cookies: driver.add_cookie(cookie)
-
-        # if no cookies, try to login
-        else:
-            self.fb_login(driver = driver)
-            time.sleep(random.uniform(3,9))
-
-        # retrieve the session id (apparently needed for profile.scrape...)
-        SESSIONID = driver.session_id
-
-        headers = {'User-Agent': "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.57",
-                   "cookie": f"sessionid={SESSIONID}"}
-
-                # instantiate insta-scrape Profile class
         # check that https://www.instagram.com does NOT precede it
         # username str sub for insta
         if username.startswith("https://www.instagram.com/"):
             username = username.replace("https://www.instagram.com/", "")
             username = username.replace("/", "")
-
-        insta_profile = Profile(username)
-        # insta_profile.scrape(headers = headers)
-        insta_profile.scrape(webdriver=driver)
 
         # now scrape posts url
         # also setting a pause between requests to avoid blocking
@@ -207,65 +94,66 @@ class InstagramHarvester(BaseHarvester):
         if incremental:
             since_id = self.state_store.get_state(__name__, u"timeline.{}.since_id".format(username))
 
+        # create cookiejar if not exits
+        if not os.path.exists("cookie_jar_instascrape"):
+            user_email = self.message['credentials']['user_email_ins']
+            user_password = self.message['credentials']['user_password_ins']
+            login = {"login_user": user_email, "login_pass": user_password}
+            insta_scraper = instagram_scraper.InstagramScraper(**login)
+            insta_scraper.authenticate_with_login()
+
+            # save cookies
+            if insta_scraper.session.cookies:
+                with open("cookie_jar_instascrape", "wb") as f:
+                    pickle.dump(insta_scraper.session.cookies, f)
+            else:
+                log.error("Could not find cookies after logging in. Stopping.")
+                return
+
+        # start scraping by instantiating sesseion class
+        insta_scraper = instagram_scraper.InstagramScraper(cookiejar="cookie_jar_instascrape")
+        # get info
+        shared_info = insta_scraper.get_shared_data_userinfo(username)
+
         posts = []
+        counter = 0
         # this gets the post ids so that they later can be scraped AND checks whether the post was scraped beforehand
         # amount = 5 or something else can be used to limit post number
-        for post in insta_profile.get_posts(webdriver = driver, scrape_pause = 5, max_failed_scroll = 400):
+        for post in insta_scraper.query_media_gen(shared_info):
 
             self.result.harvest_counter["posts"] += 1
             self.result.increment_stats("posts")
-
+            counter += 1
             # for very long harvests, try to avoid blocking by sleeping after a
             # certain amount of posts
-            if self.result.harvest_counter["posts"] in [150, 400, 800, 1600, 2400]:
+            if self.result.harvest_counter["posts"] in [150, 400, 800]:
                 log.info("Waiting a few minutes to avoid block bc of too many requests")
                 time.sleep(random.uniform(50, 450))
-
-            if incremental and post["source"] == since_id and post["source"]:
-                log.info("Stopping, found last post that was previously harvested with id: %s", post["source"])
+            # if posts > 1000 for that harvest we just break to avoid blocking!
+            if counter > 1000:
+                log.info("Reached 1000 Insta posts. Stopping.")
                 break
 
+            if incremental and post.get("id") == since_id:
+                log.info("Stopping, found last post that was previously harvested with id: %s", post["source"])
+                break
+            # just append the whole dict
             posts.append(post)
+            # add random sleep to avoid blocks
+            time.sleep(0.2, 2)
 
-        # ready to scrape individual posts
-        # will scrape individual posts with selenium - pause is set even higher here
-        # with 1 post = 10 seconds - very active insta pages will thus
-        # take a very long time
-        log.info("Scraping Posts")
-        time.sleep(10)
-        # todo - should work without this!
-        # scraped_posts, unscraped_posts = scrape_posts(posts, webdriver = driver, pause = 10, silent = False)
-        # all_posts = [post.to_dict() for post in scraped_posts]
-        # print(all_posts)
-
-        all_posts = scrape_posts(posts, webdriver = driver, pause = 5, silent = False)
-        # print(all_posts)
-        # scrape_posts()[1] would be failed scraped posts
-        all_posts = all_posts[0]
-        # print(all_posts)
-
-        # make sure the driver always quits otherways it will keep open
-        # and cause problems the next time around (https://www.youtube.com/watch?v=O_I6TJAKvH8)
-        driver.quit()
-
-        for post in all_posts:
 
             # media is captured by warcprox
-            if harvest_media and post['display_url']:
-                self._harvest_media_url(post['display_url'])
-                time.sleep(random.uniform(3,9))
-
-        all_posts = [post.to_dict() for post in all_posts]
+            if harvest_media and post['thumbnail_src']:
+                self._harvest_media_url(post['thumbnail_src'])
+                time.sleep(random.uniform(2,5))
 
         # finally set state for incremental harvests
         if incremental:
             key = "timeline.{}.since_id".format(username)
-            max_post_id = all_posts[0]["shortcode"] # first post should be most recent one
+            max_post_id = posts[0]["id"] # first post should be most recent one
             self.state_store.set_state(__name__, key, max_post_id)
             log.info("Wrote first, most recent scraped post to state_store: %s (state: %s)", max_post_id, key)
-        # rationale for state:
-        # todo
-
 
         # write content to separate warc (same strategy as with facebook scraper)
         random_token = ''.join(random.sample('abcdefghijklmnopqrstuvwxyz0123456789', 8))
@@ -292,6 +180,9 @@ class InstagramHarvester(BaseHarvester):
                                                 warc_content_type = "application/json")
             writer.write_record(record)
             log.info("Writing scraped results to %s", self.warc_temp_dir)
+            # sleep between harvests
+            log.info("Sleeping a few minutes to avoid too many subsequent harvests.")
+            time.sleep(random.randint(850, 1500))
 
     def _harvest_media_url(self, url):
         log.debug("Harvesting media URL %s", url)
